6e158ea3585a3cb877ef52a08b07148adeccb3ae
64J0 <vinigaio97@gmail.com>
2021-05-16 19:22:29 -0300

    feat: update codebase with a new example,

---
+++ test_operations/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // addition
+    let sum: u8 = 5 + 8;
+    println!("The value of sum is {}", sum);
+
+    // division
+    let quotient: f64 = 56.7 / 32.2;
+    println!("The value of quotient is {}", quotient);
+
+    // remainder
+    let remainder: u8 = 43 % 5;
+    println!("The value of remainder is {}", remainder);
+}
