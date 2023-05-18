1a48b13cc63ebebee52aa726ba2737eef2deacee
64J0 <vinigaio97@gmail.com>
2023-05-18 16:27:08 -0300

    chore: improve difference_of_squares solution

--- exercism/difference_of_squares/src/main.rs
+++ exercism/difference_of_squares/src/main.rs
@@ -2,30 +2,20 @@ fn main() {
     let n = 10;
 
     println!("The square of sum: {}", square_of_sum(&n));
-    println!("The sum of square: {}", sum_of_square(&n));
+    println!("The sum of square: {}", sum_of_squares(&n));
 
     let d = difference(&n);
     println!("The difference is: {}", d);
 }
 
 fn square_of_sum(n: &u32) -> u32 {
-    let mut result = 0;
-
-    for i in 1..(*n + 1) {
-        result += i;
-    }
-
-    u32::pow(result, 2)
+    let limit = n + 1;
+    (1..limit).sum::<u32>().pow(2)
 }
 
 fn sum_of_squares(n: &u32) -> u32 {
-    let mut result = 0;
-
-    for i in 1..(*n + 1) {
-        result += u32::pow(i, 2);
-    }
-
-    result
+    let limit = n + 1;
+    (1..limit).fold(0, |sum, x| sum + u32::pow(x, 2))
 }
 
 fn difference(n: &u32) -> u32 {
