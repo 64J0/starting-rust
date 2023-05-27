23710fd4934404abd791a5919f06a79ee5ba7e37
64J0 <vinigaio97@gmail.com>
2023-05-27 14:48:38 -0300

    chore: reorganize restaurante project example

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,25 +1,12 @@
-#[allow(dead_code)]
+mod front_of_house;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    // front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
